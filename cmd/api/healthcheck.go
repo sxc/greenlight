27823256753141edd776e5cefc89949547a9ed92
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-06-01 09:35:26 +0800

    add .gitignore

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status":"available", "environment": %q,", "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.Write(js)
+}
+
+// Define a writeJSON() helper for sending responses.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
